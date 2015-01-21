c3794b872d1bfbdc52a6be83234829058c3e44ab
Oleg <o.yanchinskiy@gmail.com>
2015-01-21 15:36:48 +0300

    updated with Windows shortcuts

--- shortcuts.js
+++ shortcuts.js
@@ -1,3 +1,6 @@
+// WARNING ALL THE WINDOWS COMMANDS ARE UNTESTED - USE AT YOUR OWN RISK
+// IF YOU FIND THAT ONE OF THE SHORTCUTS DOES NOT WORK, PLEASE UPDATE FOR FUTURE PEERS
+
 // Alfred:
 // open an app
 // search google
@@ -15,7 +18,7 @@
 // + (while open) : mark as important
 // r : reply
 // a : reply all
-// # (while open) : delete
+// # (while open) : delete + edit
 // u : return to main window
 
 // OSX:
@@ -24,42 +27,69 @@
 // command + q (while in open applications) : quit an application
 
 // Chrome / Sublime / and Bash:
-// command + n : create new window
+// command + n : create new window 
+// WINDOWS: ctrl + n
 // command + t : create new tab
+// WINDOWS: ctrl + t
 // command + w : close tab
+// WINDOWS: ctrl + w
 // command + q : quit application
+// WINDOWS: ctrl + q
 // command + shift + ] : move right one tab
+// WINDOWS: ctrl + shift + ]
 // command + shift + [ : move left one tab
+// WINDOWS: ctrl + shift + [
 // command + NUMBER : move to tab number NUMBER (not in Bash)
+// WINDOWS: ctrl + (1 - 8)
 
-// Chrome:
-// pin tabs
+// Chrome: 
 // command + r : reload page
+// WINDOWS: ctrl + r 
 // command + option + j : open the javascript console
+// WINDOWS: ctrl + shift + j
 // command + shift + i : email link for this page
+// WINDOWS: ctrl + shift + i
 // command + arrows : move through history
+// WINDOWS: BACKSPACE to go to move backwards, SHIFT + BACKSPACE or ALT + Right Arrow to move forwards
 // command + shift + t : reopen last closed tab (up to 10)
+// WINDOWS: ctrl + shift + t
 
 // Sublime:
 // command + ctrl + f : toggle fullscreen
+// WINDOWS: F11
 // command + ctrl + shift + f : toggle distraction free
-
+// WINDOWS: SHIFT + F11
 // option + arrows : move one word at a time
+// WINDOWS: ctrl + arrows 
 // command + arrows : move to beginning and end of line
+// WINDOWS: HOME or END
 // shift + above commands : select all the text
-// delete + the above : delete all the text
+// WINDOWS: ????
+// delete + edit + the above : delete + edit all the text
+// WINDOWS: ????
 // fn + delete : delete forward
-
+// WINDOWS: DELETE 
 // command + z : undo
+// WINDOWS: ctrl + z
 // command + shift + z : redo
+// WINDOWS: ctrl + y
 // command + / : toggle comment for line
+// WINDOWS: ctrl + / 
 // command + ] : shift indenting right
+// WINDOWS: ctrl + ]
 // command + [ : shift indenting left
+// WINDOWS: ctrl + []
 // command + l : select the whole line
+// WINDOWS: ctrl + l
 // command + c (with selection) : copy selection
+// WINDOWS: ctrl + c  
 // command + x (with selection) : cut selection
+// WINDOWS: ctrl + x 
 // command + v : paste most recent cut or copy
+// WINDOWS: ctrl + v
 // command + shift + v : paste with proper indentation
+// WINDOWS: ctrl + shift + v
+
 
 var outerFunction = function(){
 
@@ -73,32 +103,48 @@ var innerFunction = function(){
 
 
 // command + x : cut the whole line
+// WINDOWS: ctrl + x
 // command + c : copy the whole line
+// WINDOWS: ctrl + c 
 // command + enter : create new line below
+// WINDOWS: enter
 // command + shift + enter : create new line above
+// WINDOWS: ctrl + shift + enter
 // command + shift + d : copy current line below
+// WINDOWS: ctrl + shift + d 
 // command + control + arrows : move entire line up and down
+// WINDOWS: ctrl + shift + arrows
 // command + k, command + u : make selection upper case
+// WINDOWS: Ctrl + K + U: Convert to upper
 // command + k, command + l : make selection lower case
-// command + ' : select all within quotes
-var quote = "Wouldn't it be nice to replace this quote?";
-// command + shift + ' : select all within quotes, and quotes
-var quote = "Wouldn't it be nice to replace this quote with a number?";
-// control + m : move to beginning/end of parens and braces
-if('only there were a way to toggle around the parens'){
-  var it = 'would be rad';
-}
+// WINDOWS: CTrl + K + L: Covert to lower
+
 
 // command + d : select word(s)
-// command + k while selecting words : don't select word 
+// WINDOWS: ctrl + d
+// command + k while selecting words : delete + edit
+// WINDOWS:  ctrl + k
 // command + ctrl + g : select all of words
+// WINDOWS: ????
 // option + mouseDrag : column select
+// WINDOWS: ctrl + left click 
 // command + mouseClick : custom multiple cursor
+// WINDOWS: ctrl + left click
 
 // command + p : open file
+// WINDOWS: ctrl + O || ctrl + p
 // command + shift + p : set syntax (and many many other things)
+// WINDOWS:  Ctrl-Shift-P
 // command + f : find
+// WINDOWS: ctrl + f 
 // command + shift + f : massive find
+// WINDOWS: ctrl + shift + f
 // command + k, command + b : toggle file bar
+// WINDOWS: ctrl + K, ctrl + b
 // command + option + NUMBER : split screen
-// control + shift + NUMBER : move current file to pane
+// WINDOWS: Alt + shift + 2	Split view into two columns
+// WINDOWS: Alt + shift + 1	Revert view to single column
+// command + option + 5: split into grid(4 groups)
+// WINDOWS: Alt + shift + 5	Set view to grid (4 groups)
+
+
